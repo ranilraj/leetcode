d32e211c7e5a890743dbf97dd0f020e7377cac8a
ranil <ranilraj18@gmail.com>
2024-09-19 22:09:48 +0530

    initial idea

--- leetcode_2739.py
+++ leetcode_2739.py
@@ -1,2 +1,12 @@
-mainTank = 5
-additionalTank = 10
+mainTank = 1
+additionalTank = 2
+distance=0
+if mainTank<5:
+    print(mainTank*10)
+    exit()
+else:
+    while mainTank>=5 and additionalTank!=0:
+        mainTank-=5
+        distance+=60
+        additionalTank-=1
+print(distance)
