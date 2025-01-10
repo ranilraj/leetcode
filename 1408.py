8b06e7bc204879be69126696e6bd3d20396d75f4
ranil <ranilraj18@gmail.com>
2025-01-10 11:51:28 +0530

    done iteration

--- 1408.py
+++ 1408.py
@@ -1,1 +1,9 @@
 words = ["mass","as","hero","superhero"]
+a=[]
+for i in range(len(words)):
+        for j in range(len(words)):
+            print(words[i])
+            print(words[j])
+            if words[i] in words[j] and i != j:
+                  a.append(words[i])
+print(a)
